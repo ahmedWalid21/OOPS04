7e3442fa62a82d742aa365d9c7a93e55ded692bb
ahmedWalid21 <420230110@fa-hists.edu.eg>
2025-07-30 17:31:35 +0300

    V03

---
+++ OOPS04/Operator overrloading/Complexx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS04.Operator_overrloading
+{
+    internal class Complexx
+    {
+        public int Real {  get; set; }
+        public int Imag {  get; set; }
+        public override string ToString()
+        {
+            return $"{Real} + {Imag} i";
+        }
+
+        public static Complexx operator +(Complexx right, Complexx left)
+        {
+            return new Complexx()
+            {
+                
+            };
+        }
+
+        public static Complexx operator -(Complexx right, Complexx left)
+        {
+            return new Complexx()
+            {
+                Real =( right?.Real ?? 0) -( left?.Real ?? 0),
+                Imag = (right?.Imag ?? 0 )- (left?.Imag ?? 0)
+            };
+        }
+
+
+
+    }
+}
--- OOPS04/Program.cs
+++ OOPS04/Program.cs
@@ -1,24 +1,35 @@
-﻿using System.Drawing;
-using OOPS04.Abstraction;
+﻿
+
+using OOPS04.Operator_overrloading;
 
 namespace OOPS04
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             //Abstraction:
             //Abstract: C# keyword[method - class - properties]
 
-           /// Rectanglee r = new Rectanglee() { dim01 = 2, dim02 = 13 };
-           // Console.WriteLine(r.GetArea());
-           // Console.WriteLine(r.Perimeter);
-            
+            /// Rectanglee r = new Rectanglee() { dim01 = 2, dim02 = 13 };
+            // Console.WriteLine(r.GetArea());
+            // Console.WriteLine(r.Perimeter);
+
 
             //interface VS Abstract Class
 
 
+            //operator overloading
 
+
+          
+
+            Complexx c = new Complexx() { Real = 5, Imag = 4 };
+            Complexx c2 = new Complexx() { Real = 3, Imag = 1 };
+            Console.WriteLine(c);
+            Console.WriteLine(c2);
+            Complexx c3 = c -null;
+            Console.WriteLine(c3);
 
 
         }
